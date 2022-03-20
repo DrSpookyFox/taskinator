e89a4cbe4444a2364c620f50343ecef31ed726fa
Alexander Singer <alexandersinger@Alexanders-Air.fios-router.home>
2022-03-20 16:50:45 -0400

    added for loop for deleting tasks

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,12 @@
+
+
 var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
 
 var taskFormHandler = function(event) {
 
@@ -20,7 +23,8 @@ var taskFormHandler = function(event) {
       else {
         var taskDataObj = {
           name: taskNameInput,
-          type: taskTypeInput
+          type: taskTypeInput,
+          status: "to do"
         };
       
         createTaskEl(taskDataObj);
@@ -65,7 +69,14 @@ var createTaskEl = function(taskDataObj) {
 
     tasksToDoEl.appendChild(listItemEl);
 
+    taskDataObj.id = taskIdCounter;
+
+    tasks.push(taskDataObj);
+
     taskIdCounter++;
+
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
 
 };
 
@@ -129,6 +140,19 @@ pageContentEl.addEventListener("click", taskButtonHandler);
         var deleteTask = function(taskId) {
             var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
             taskSelected.remove();
+
+            var updatedTaskArr = [];
+
+            // loop through current tasks
+            for (var i = 0; i < tasks.length; i++) {
+            // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+            if (tasks[i].id !== parseInt(taskId)) {
+                updatedTaskArr.push(tasks[i]);
+                 }
+             }
+
+            // reassign tasks array to be the same as updatedTaskArr
+            tasks = updatedTaskArr;
              }
 
         var editTask = function(taskId) {
@@ -154,6 +178,13 @@ var completeEditTask = function(taskName, taskType, taskId) {
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
 
+    for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+          tasks[i].name = taskName;
+          tasks[i].type = taskType;
+        }
+      };
+
     alert("Task Updated!");
 
     formEl.removeAttribute("data-task-id");
@@ -175,6 +206,13 @@ var taskStatusChangeHandler = function(event) {
       else if (statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
       }
+
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+          tasks[i].status = statusValue;
+        }
+      }
+      console.log(tasks);
 };
 
 
